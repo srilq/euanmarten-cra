b3112a2e935f1678744b57436af78a39a0437dd6
Stef <git@srilq.email>
2019-06-15 21:39:04 +0100

    add gallery

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import 'tachyons-verbose';
+import './Gallery.css';
+
+const galleryConfig = {
+  min: 1,
+  max: 4
+};
 
 function App() {
+  const [selected, setSelected] = useState(1);
+
+  const isSelectedMin = selected === galleryConfig.min;
+  const isSelectedMax = selected === galleryConfig.max;
+
   return (
-    <div>
-      <h1>Welcome</h1>
-      <p>Hello world!</p>
+    <div className="sans-serif">
+      <header>
+        <h1>Euan Marten</h1>
+      </header>
+      <main>
+        <div><button aria-hidden disabled={isSelectedMin} onClick={isSelectedMin ? null : () => setSelected(selected - 1)}>Previous</button></div>
+        <div><button aria-hidden disabled={isSelectedMax} onClick={isSelectedMax ? null : () => setSelected(selected + 1)}>Next</button></div>
+        <div className={`Gallery-select-${selected}`}>
+          <img src="/images/oranges.jpg" alt="" />
+          <img src="/images/euan.jpg" alt="" />
+          <img src="/images/bilbo.jpg" alt="" />
+          <img src="/images/peel.jpg" alt="" />
+        </div>
+      </main>
     </div>
   );
 }

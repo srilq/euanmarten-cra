3bf7b1830db16cd4bcf765972ca8664d615083ac
Stef <git@srilq.email>
2019-06-16 11:18:03 +0100

    styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import 'tachyons-verbose';
+import 'tachyons';
+import './App.css';
 import './Gallery.css';
 
 const galleryConfig = {
@@ -14,14 +15,19 @@ function App() {
   const isSelectedMax = selected === galleryConfig.max;
 
   return (
-    <div className="sans-serif">
-      <header>
-        <h1>Euan Marten</h1>
+    <div className="sans-serif flex flex-column flex-row-l mw9 center pa4">
+      <header className="w-20-l pr4-l">
+        <h1 className="mt0">Euan Marten</h1>
+        <ul className="list pa0">
+          <li className="email"><a href="mailto:euanscottm@gmail.com">euanscottm@gmail.com</a></li>
+        </ul>
       </header>
-      <main>
-        <div><button aria-hidden disabled={isSelectedMin} onClick={isSelectedMin ? null : () => setSelected(selected - 1)}>Previous</button></div>
-        <div><button aria-hidden disabled={isSelectedMax} onClick={isSelectedMax ? null : () => setSelected(selected + 1)}>Next</button></div>
-        <div className={`Gallery-select-${selected}`}>
+      <main className="w-80-l pr5-m pr6-l">
+        <div className="flex justify-between mb3">
+          <div><button aria-hidden disabled={isSelectedMin} onClick={isSelectedMin ? null : () => setSelected(selected - 1)}>Previous</button></div>
+          <div><button aria-hidden disabled={isSelectedMax} onClick={isSelectedMax ? null : () => setSelected(selected + 1)}>Next</button></div>
+        </div>
+        <div className={`Gallery Gallery-select-${selected}`}>
           <img src="/images/oranges.jpg" alt="" />
           <img src="/images/self.jpg" alt="" />
           <img src="/images/bilbo.jpg" alt="" />

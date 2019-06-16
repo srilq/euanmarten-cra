f4f8bd3e35a9f8d6f04bd9b70283e4d04be08908
Stef <git@srilq.email>
2019-06-16 16:46:14 +0100

    make sidebar wider

--- src/App.js
+++ src/App.js
@@ -16,13 +16,13 @@ function App() {
 
   return (
     <div className="sans-serif flex flex-column flex-row-l mw8 center pa4">
-      <header className="w-20-l pr4-l pb4">
+      <header className="w-25-l pr4-l pb4">
         <h1 className="mt0">Euan Marten</h1>
         <ul className="list pa0">
           <li className="email"><a href="mailto:euanscottm@gmail.com">euanscottm@gmail.com</a></li>
         </ul>
       </header>
-      <main className="w-80-l pr5-m pr6-l">
+      <main className="w-75-l pr5-m pr6-l">
         <div className="flex justify-between mb3">
           <div><button className="pa2 lh-solid" aria-hidden disabled={isSelectedMin} onClick={isSelectedMin ? null : () => setSelected(selected - 1)}>Previous</button></div>
           <div><button className="pa2 lh-solid" aria-hidden disabled={isSelectedMax} onClick={isSelectedMax ? null : () => setSelected(selected + 1)}>Next</button></div>

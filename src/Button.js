952e9a0737620c8eb3dd9c2f7dab3aebf86eca19
Stef <git@srilq.email>
2019-06-16 23:31:53 +0100

    styles

--- src/Button.js
+++ src/Button.js
@@ -3,7 +3,7 @@ import './Button.css';
 
 const Button = ({ children, onClick, disabled, ariaHidden }) => (
   <button
-    className="Button pa2 pt1 pr3 pl3 mr0 mr3-ns ttl ba br2 b--black bg-black white"
+    className="Button pa2 pt1 pr3 pl3 mr0 mr3-ns ttl ba br1 b--black bg-black white"
     aria-hidden={ariaHidden}
     disabled={disabled}
     onClick={onClick}>
--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => (
   <header>
-    <h1 className="mt0 f5 normal mb4 ttl">Euan Marten</h1>
+    <h1 className="mt0 f2 fw2 normal mb4 ttl">Euan Marten</h1>
     <ul className="list pa0 f5 ttl">
       <li>Email: <a className="url" href="mailto:euanscottm@gmail.com">euanscottm@gmail.com</a></li>
       <li>Instagram: <a className="url" href="https://www.instagram.com/e.seilide/">e.seilide</a></li>

66bb92db83aa0b34f5bfe74561a96a3f8f90e20a
Stef <stef.jones@condenastint.com>
2019-07-13 15:25:35 +0100

    add lightbox

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'tachyons';
 import './App.css';
+import Icon from './Icon';
 import Header from './Header';
 
 const IMAGES = [
@@ -15,26 +16,32 @@ const IMAGES = [
   'sword.jpg'
 ];
 
-const Thumbnail = ({ filename, alt = '' }) => {
-  const src = `/images/${filename}`;
+const getImageSrc = filename => `/images/${filename}`;
+
+const Image = ({ image: { src, altText = '' }, ...rest }) => <img src={src} alt={altText} {...rest} />;
+
+const Thumbnail = ({ filename, setCurrentImage }) => {
+  const src = getImageSrc(filename);
 
   return (
-    <a href={src} target="_self">
-      <img src={src} alt={alt} className="w-100 db mb1 mb2-ns" />
-    </a>
+    <div className="mb1 mb2-ns">
+      <button
+        className="button-reset bn pa0 db w-100 pointer"
+        onClick={() => setCurrentImage(filename)}
+      >
+        <Image image={{ src }} className="w-100 db" />
+      </button>
+    </div>
   );
 };
 
-const ThumbnailColumn = ({ images, className }) => {
-  return (
-    <div className={`${className} ThumbnailColumn`}>
-      {images.map(filename => <Thumbnail key={filename} filename={filename} />)}
-    </div>
-  );
+const ThumbnailColumn = ({ images, className, thumbnailProps }) => (
+  <div className={`ThumbnailColumn ${className}`}>
+    {images.map(filename => <Thumbnail key={filename} filename={filename} {...thumbnailProps} />)}
+  </div>
+);
 
-}
-
-const TwoColumnLayout = ({ images }) => {
+const TwoColumnLayout = ({ images, thumbnailProps }) => {
   const firstColumnImages = [];
   const secondColumnImages = [];
 
@@ -49,21 +56,62 @@ const TwoColumnLayout = ({ images }) => {
 
   return (
     <div className="flex flex-row">
-      <ThumbnailColumn images={firstColumnImages} className="w-50 pa1 pa2-ns" />
-      <ThumbnailColumn images={secondColumnImages} className="w-50 pa1 pa2-ns pl0 pl0-ns" />
+      <ThumbnailColumn
+        images={firstColumnImages}
+        className="w-50 pa1 pa2-ns"
+        thumbnailProps={thumbnailProps}
+      />
+      <ThumbnailColumn
+        images={secondColumnImages}
+        className="w-50 pa1 pa2-ns pl0 pl0-ns"
+        thumbnailProps={thumbnailProps}
+      />
     </div>
   );
 };
 
-const App = () => (
-  <div className="sans-serif">
-    <div className="pa4">
-      <Header />
+const Lightbox = ({ isOpen, filename, onClose }) => {
+  if(!isOpen || !filename) return null;
+
+  const src = getImageSrc(filename);
+
+  return (
+    <div className="Lightbox fixed absolute--fill bg-near-black pt5 pb5 pr3 pl3 pt4-l pb4-l pr5-l pl5-l">
+      <Image image={{ src }} />
+      <buton
+        className="pointer pa3 absolute top-0 right-0 right-1-ns"
+        onClick={e => {
+          e.preventDefault();
+          onClose && onClose();
+        }}
+      >
+        <Icon name="CLOSE" className="white" />
+      </buton>
     </div>
-    <main>
-      <TwoColumnLayout images={IMAGES} />
-    </main>
-  </div>
-);
+  );
+};
+
+const App = () => {
+  const [currentImage, setCurrentImage] = useState(null);
+
+  return (
+    <div className="sans-serif">
+      <div className="pa4">
+        <Header />
+      </div>
+      <main>
+        <TwoColumnLayout
+          images={IMAGES}
+          thumbnailProps={{ setCurrentImage }}
+        />
+      </main>
+      <Lightbox
+        isOpen={!!currentImage}
+        filename={currentImage}
+        onClose={() => setCurrentImage(null)}
+      />
+    </div>
+  );
+};
 
 export default App;

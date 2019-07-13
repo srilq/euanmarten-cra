2fa742aa5994d3ff1ed243a15b9bb5785171bba0
Stef <stef.jones@condenastint.com>
2019-07-13 13:54:26 +0100

    add social media icons

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Icon from './Icon';
+
+const IconLink = ({ name, href, children }) => (
+  <a href={href} className="flex ml2"><Icon name={name} className={children && 'mr2'} />{children}</a>
+);
 
 const Header = () => (
-  <header>
-    <h1 className="mt0 f2 fw2 normal mb4 ttl">Euan Marten</h1>
-    <ul className="list pa0 f5 ttl">
-      <li>Email: <a className="url" href="mailto:euansmarten@gmail.com">euansmarten@gmail.com</a></li>
-      <li>Instagram: <a className="url" href="https://www.instagram.com/e.seilide/">e.seilide</a></li>
+  <header className="flex flex-row justify-between items-center">
+    <h1 className="ma0 f2 fw2 normal ttl">Euan Marten</h1>
+    <ul className="list pa0 ma0 f5 ttl flex flex-row">
+      <li><IconLink href="https://www.instagram.com/e.seilide/" name="INSTAGRAM" /></li>
+      <li><IconLink href="https://e-seilide.tumblr.com" name="TUMBLR" /></li>
+      <li><IconLink href="http://twitter.com/e_seilide" name="TWITTER" /></li>
+      <li><IconLink href="mailto:euansmarten@gmail.com" name="EMAIL"><span className="lh-title">euansmarten@gmail.com</span></IconLink></li>
     </ul>
   </header>
 );

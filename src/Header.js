e7b9fd29273ed23ba2ce5350a240fab2f04cc7a0
Stef <stef.jones@condenastint.com>
2019-07-13 13:17:31 +0100

    Change email address

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ const Header = () => (
   <header>
     <h1 className="mt0 f2 fw2 normal mb4 ttl">Euan Marten</h1>
     <ul className="list pa0 f5 ttl">
-      <li>Email: <a className="url" href="mailto:euanscottm@gmail.com">euanscottm@gmail.com</a></li>
+      <li>Email: <a className="url" href="mailto:euansmarten@gmail.com">euansmarten@gmail.com</a></li>
       <li>Instagram: <a className="url" href="https://www.instagram.com/e.seilide/">e.seilide</a></li>
     </ul>
   </header>

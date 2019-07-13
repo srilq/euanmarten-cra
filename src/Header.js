126fcd622d06bff91474f95a98326da0d2c6e88f
Stef <stef.jones@condenastint.com>
2019-07-13 14:01:43 +0100

    fix mobile view

--- src/Header.js
+++ src/Header.js
@@ -2,17 +2,19 @@ import React from 'react';
 import Icon from './Icon';
 
 const IconLink = ({ name, href, children }) => (
-  <a href={href} className="flex ml2"><Icon name={name} className={children && 'mr2'} />{children}</a>
+  <a href={href} className="flex ml2"><Icon name={name} />
+    {children && <span className="dn di-l ml2 lh-title">{children}</span>}
+  </a>
 );
 
 const Header = () => (
-  <header className="flex flex-row justify-between items-center">
-    <h1 className="ma0 f2 fw2 normal ttl">Euan Marten</h1>
+  <header className="flex flex-column flex-row-ns justify-between items-center">
+    <h1 className="tc tl-ns ma0 mb4 mb0-ns mr4-ns f2 fw2 normal ttl">Euan Marten</h1>
     <ul className="list pa0 ma0 f5 ttl flex flex-row">
       <li><IconLink href="https://www.instagram.com/e.seilide/" name="INSTAGRAM" /></li>
       <li><IconLink href="https://e-seilide.tumblr.com" name="TUMBLR" /></li>
       <li><IconLink href="http://twitter.com/e_seilide" name="TWITTER" /></li>
-      <li><IconLink href="mailto:euansmarten@gmail.com" name="EMAIL"><span className="lh-title">euansmarten@gmail.com</span></IconLink></li>
+      <li><IconLink href="mailto:euansmarten@gmail.com" name="EMAIL">euansmarten@gmail.com</IconLink></li>
     </ul>
   </header>
 );

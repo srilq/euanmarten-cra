ea71bfe6310f0f1f071c3e734a89fe7c9ce9b2eb
Stef <git@srilq.email>
2019-08-17 12:18:26 +0100

    Add images.json

--- scripts/optimise-images.js
+++ scripts/optimise-images.js
@@ -1,37 +1,8 @@
 #!/usr/bin/env node
-const path = require('path');
 const fs = require('fs').promises;
 const del = require('del');
 const createLoggerFn = require('./utils/logger');
 const createImageRenditions = require('./utils/create-image-renditions');
-
-const config = {
-  inputDir: path.join('build', 'images'),
-  outputDir: path.join('build', 'images', 'renditions'),
-  concurrency: 20,
-  fileTypes: ['jpg', 'jpeg', 'png'],
-  compressionTypes: ['jpeg', 'png'],
-  defaultCompression: 'jpeg',
-  quality: {
-    // JPEG compression (lossy)
-    // higher number means better quality
-    // but larger file size
-    jpeg: 90,
-    // PNG commpression (lossless)
-    // higher number means smaller file size
-    // but it takes longer
-    png: 100
-  },
-  resizeOptions: '>',
-  renditions: [
-    {
-      width: 100
-    },
-    {
-      width: 200
-    }
-  ],
-};
 
 const logger = {
   info: createLoggerFn(console.info, '  '),
@@ -39,8 +10,10 @@ const logger = {
   error: createLoggerFn(console.error, '🚨')
 };
 
-const exec = async config => {
+const exec = async () => {
   try {
+    const configJson = await fs.readFile('images.json');
+    const config = JSON.parse(configJson);
     await del(config.outputDir);
     await fs.mkdir(config.outputDir, { recursive: true });
     await createImageRenditions({
@@ -55,4 +28,4 @@ const exec = async config => {
   }
 };
 
-exec(config);
+exec();
